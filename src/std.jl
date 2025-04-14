c0eeee30977c4e1323c7fa0d80785fe340d39738
Jimmy Envall <jimmyenvall@gmail.com>
2025-04-14 23:15:14 +0200

    Simplify in to_std_string

--- src/std.jl
+++ src/std.jl
@@ -1091,7 +1091,7 @@ to_std_string(gradient(x' * A * x, x))
 ```
 """
 function to_std_string(arg)
-    arg = evaluate(arg)
+    arg = simplify(arg)
     free_indices = unique(get_free_indices(arg))
 
     standardized = if length(free_indices) == 2
