bf36482c2f9987f3ed53c62cd5ac000d466e7962
Jimmy Envall <jimmyenvall@gmail.com>
2025-04-23 09:54:43 +0200

    Check only free indices in common

--- src/forward.jl
+++ src/forward.jl
@@ -94,8 +94,8 @@ function evaluate(::Mult, arg1::Real, arg2::BinaryOperation{Mult})
 end
 
 function indices_in_common(arg1, arg2)
-    arg1_indices = get_indices(arg1)
-    arg2_indices = get_indices(arg2)
+    arg1_indices = get_free_indices(arg1)
+    arg2_indices = get_free_indices(arg2)
 
     return intersect(arg1_indices, arg2_indices)
 end
