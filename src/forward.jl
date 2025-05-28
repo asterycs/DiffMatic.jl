54e4312508c8a6acef3158b8f6e6894c5ad5ef01
Jimmy Envall <jimmyenvall@gmail.com>
2025-05-28 19:39:20 +0200

    Delete unreachable branch

--- src/forward.jl
+++ src/forward.jl
@@ -94,8 +94,6 @@ function evaluate(::Mult, arg1::Real, arg2::BinaryOperation{Mult})
 
     if arg2.arg1 isa Real
         return BinaryOperation{Mult}(arg1 * arg2.arg1, arg2.arg2)
-    elseif arg2.arg2 isa Real
-        return BinaryOperation{Mult}(arg1 * arg2.arg2, arg2.arg1)
     end
 
     return BinaryOperation{Mult}(arg1, evaluate(arg2))
