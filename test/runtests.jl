d5a96a83f8c1b80668ff3a7b106a457ba5623e00
Jimmy Envall <jimmy.envall@inf.ethz.ch>
2025-11-19 10:15:15 +0100

    CI: Run all test items

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using Documenter
-using Test
 using TestItems
+using TestItemRunner
 
 @testitem "Aqua" begin
     include("TestUtils.jl")
@@ -42,5 +41,10 @@ end
     include("JuliaTest.jl")
 end
 
-DocMeta.setdocmeta!(DiffMatic, :DocTestSetup, :(using DiffMatic); recursive = true)
-doctest(DiffMatic)
+@testitem "DocTest" begin
+    using Documenter
+    DocMeta.setdocmeta!(DiffMatic, :DocTestSetup, :(using DiffMatic); recursive = true)
+    doctest(DiffMatic)
+end
+
+@run_package_tests
